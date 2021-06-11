ae930d6205f4a6209760bab95800f3ce4a4a5686
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-06-11 12:54:51 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ⊖  "
-        await event.edit("**⚡Geez - Project⚡**\n\n"
+        await event.edit("**卍 Stephanie - Project 卍**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉ {string}◉\n\n")
