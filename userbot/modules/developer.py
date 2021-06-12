0ef18f3ee9228a020c2f0f641aa818b6acc5442e
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-12 12:01:39 +0700

    Created developer
    • Membuat developer ( Untuk Mengecek Daftar Developer Pada Userbot )
    Created by Apis

---
+++ userbot/modules/developer.py
@@ -0,0 +1,27 @@
+# Created by Apis 
+# Jan hapus credit ya anjing!!!
+
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.developer(?: |$)(.*)")
+async def _(event):
+    event.pattern.match_group(1)
+    await event.edit(
+        f"__Berikut Developer Repo Userbot Kami :__ "
+        f"• [Arya Zakaria](https://t.me/Badboyanim) "
+        f"• [Apis](https://t.me/PacarFerdilla) \n\n"
+        f" - - - - - - - - - - - - - - - - - -  \n"
+        f" - - S T E P H A N I E U S E R B O T - - \n"
+        f" - - - - - - - - - - - - - - - - - - ")
+
+CMD_HELP.update(
+    {
+        "developer": "__**Modules**__ `developer`\
+        \n\n  •  __**Command**__ :** `.developer`\
+        \n  •  __Penjelasan : __Untuk Mengecek Daftar Developer Pada Userbot\
+    "
+    }
+)
