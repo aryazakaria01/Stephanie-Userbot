b176ccc44c5ca47e56654fe70e9ae88787fa5115
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-14 12:21:08 +0700

    Update statme
    • Statme , for updating ( 🗿 ) ( emot batu ) hahahaha....

--- userbot/modules/statme.py
+++ userbot/modules/statme.py
@@ -70,22 +70,22 @@ async def stats(event: NewMessage.Event) -> None:  # pylint: disable = R0912, R0
         unread_mentions += dialog.unread_mentions_count
         unread += dialog.unread_count
     stop_time = time.time() - start_time
-
+    # Edit by Apis
     full_name = inline_mention(await event.client.get_me())
-    response = f'卍 **Stats for {full_name}** \n\n'
-    response += f'**Private Chats:** {private_chats} \n'
+    response = f'🗿**Stats for {full_name} 🗿** \n\n'
+    response += f'🗿**Private Chats 🗿:** {private_chats} \n'
     response += f'   卍 `Users: {private_chats - bots}` \n'
     response += f'   卍 `Bots: {bots}` \n'
-    response += f'**Groups:** {groups} \n'
-    response += f'**Channels:** {broadcast_channels} \n'
-    response += f'**Admin in Groups:** {admin_in_groups} \n'
+    response += f'🗿**Groups 🗿:** {groups} \n'
+    response += f'🗿**Channels 🗿:** {broadcast_channels} \n'
+    response += f'🗿**Admin in Groups 🗿:** {admin_in_groups} \n'
     response += f'   卍 `Creator: {creator_in_groups}` \n'
     response += f'   卍 `Admin Rights: {admin_in_groups - creator_in_groups}` \n'
-    response += f'**Admin in Channels:** {admin_in_broadcast_channels} \n'
+    response += f'🗿**Admin in Channels 🗿:** {admin_in_broadcast_channels} \n'
     response += f'   卍 `Creator: {creator_in_channels}` \n'
     response += f'   卍 `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n'
-    response += f'**Unread:** {unread} \n'
-    response += f'**Unread Mentions:** {unread_mentions} \n\n'
+    response += f'🗿**Unread 🗿:** {unread} \n'
+    response += f'🗿**Unread Mentions 🗿:** {unread_mentions} \n\n'
     response += f'__It Took:__ {stop_time:.02f}s \n'
 
     await event.edit(response)
